2dfecc3e9a3dbb123f6e7c8a3642f8aaee2f36d1
Coenraad Human <coen.human@gmail.com>
2019-08-21 18:36:38 +0200

    Log out working and default theme is cosmic!!!

--- WeatherWebApplication/src/app/@theme/theme.module.ts
+++ WeatherWebApplication/src/app/@theme/theme.module.ts
@@ -82,7 +82,7 @@ export class ThemeModule {
       providers: [
         ...NbThemeModule.forRoot(
           {
-            name: 'default',
+            name: 'cosmic', // set default theme on startup
           },
           [ DEFAULT_THEME, COSMIC_THEME, CORPORATE_THEME, DARK_THEME ],
         ).providers,
