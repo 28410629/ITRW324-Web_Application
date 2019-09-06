5d24dad9557d8ec28f8808256145c094a6f4e352
Coenraad Human <coen.human@gmail.com>
2019-09-06 15:37:20 +0200

    Moved Morne's work over to the new component.

--- WeatherWebApplication/src/app/pages/user-pages/favourite-stations/favourite-stations.component.ts
+++ WeatherWebApplication/src/app/pages/user-pages/favourite-stations/favourite-stations.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AverageReadingService} from '../../../services/AverageReadingService';
+import {AverageReadingEntity} from '../../../models/averagereadings.model';
 
 @Component({
   selector: 'ngx-favourite-stations',
   templateUrl: 'favourite-stations.component.html',
   styleUrls: ['favourite-stations.component.scss'],
 })
-export class FavouriteStationsComponent {
-  constructor() {}
+export class FavouriteStationsComponent implements OnInit {
+  statusReadings: AverageReadingEntity[] = [];
+
+  constructor(private service: AverageReadingService) {}
+
+  ngOnInit(): void {
+    this.service.FetchAverageToday('2347795-10359807-10359964').subscribe(
+      data => {
+        this.statusReadings = data.avgReadings;
+      },
+      error => {
+      },
+    );
+  }
 }
