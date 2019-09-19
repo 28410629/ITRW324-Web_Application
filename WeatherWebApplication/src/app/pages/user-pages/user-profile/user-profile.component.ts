3981d9740d7e865d6817e719074c8524bd206993
Coenraad Human <coen.human@gmail.com>
2019-09-19 15:34:37 +0200

    Can upload profile image!

--- WeatherWebApplication/src/app/pages/user-pages/user-profile/user-profile.component.ts
+++ WeatherWebApplication/src/app/pages/user-pages/user-profile/user-profile.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {User} from '../../../models/user.model';
 import {AuthService} from '../../../auth/auth-service.service';
+import {AngularFireStorage} from '@angular/fire/storage';
+import {finalize} from 'rxjs/operators';
 
 @Component({
   selector: 'ngx-user-profile',
@@ -8,6 +10,14 @@ import {AuthService} from '../../../auth/auth-service.service';
   styleUrls: ['user-profile.component.scss'],
 })
 export class UserProfileComponent {
+
+  // Photo upload
+  ref;
+  task;
+  uploadProgress;
+  downloadURL;
+  photoURL =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQv3-pAMsgi3CZrot52SIgT8Ub0hQNpDZ5ZVkT-Pef7usIaGtNXAg';
 
   cardHeader;
   isLoaded: boolean = false;
@@ -19,11 +29,34 @@ export class UserProfileComponent {
   email;
   useruid;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,
+              private afStorage: AngularFireStorage) {
     const storageuser: User = JSON.parse(localStorage.getItem('user'));
     this.user = storageuser;
     this.useruid = this.user.uid;
     this.getUserData();
+  }
+
+  submitChanges() {
+    this.authService.UpdateUserProfile(
+      this.useruid,
+      this.name,
+      this.photoURL);
+  }
+
+  uploadPhoto(event) {
+    this.ref = this.afStorage.ref('users/' + this.user.uid + '/' + event.target.files[0].name);
+    this.task = this.ref.put(event.target.files[0]);
+
+    this.uploadProgress = this.task.percentageChanges();
+    this.task.snapshotChanges().pipe(
+      finalize(() => {
+        this.ref.getDownloadURL().subscribe(downloadURL => {
+          this.photoURL = downloadURL;
+          this.uploadProgress = 0;
+        });
+      }),
+    ).subscribe();
   }
 
   getUserData() {
