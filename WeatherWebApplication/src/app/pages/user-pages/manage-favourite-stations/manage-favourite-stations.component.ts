83498bce3491a9a342f06b4a7f7aaecbba96c580
Morne <morneventer.mv@gmail.com>
2019-09-19 13:09:47 +0200

    added managment of fav stations

--- WeatherWebApplication/src/app/pages/user-pages/manage-favourite-stations/manage-favourite-stations.component.ts
+++ WeatherWebApplication/src/app/pages/user-pages/manage-favourite-stations/manage-favourite-stations.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AverageReadingService} from '../../../services/AverageReadingService';
+import {AverageReadingEntity} from '../../../models/averagereadings.model';
 
 @Component({
   selector: 'ngx-manage-favourite-stations',
   templateUrl: 'manage-favourite-stations.component.html',
   styleUrls: ['manage-favourite-stations.component.scss'],
 })
-export class ManageFavouriteStationsComponent {
-  constructor() {}
+export class ManageFavouriteStationsComponent implements OnInit {
+
+  statusReadings: AverageReadingEntity[] = [];
+  isLoaded: boolean = false;
+  // time series selection
+  type = 'day';
+  types = ['day', 'week', 'month', 'year'];
+  constructor(private service: AverageReadingService) {}
+  ngOnInit(): void {
+    this.getData();
+  }
+  getData() {
+    this.isLoaded = false;
+    this.service.FetchAverageToday('2347795-10359807-10359964').subscribe(
+      data => {
+        this.statusReadings = data.avgReadings;
+        this.isLoaded = true;
+      },
+      error => {
+      },
+    );
+  }
 }
