cb7fb0b614c29475c6c04863fecf6b2fe0ba8a99
Eon VIljoen <28807995@student.g.nwu.ac.za>
2019-10-18 17:24:25 +0200

    Look of build station guide done

--- WeatherWebApplication/src/app/pages/user-pages/build-new-station/build-new-station.component.ts
+++ WeatherWebApplication/src/app/pages/user-pages/build-new-station/build-new-station.component.ts
@@ -7,11 +7,35 @@ import { Component } from '@angular/core';
 })
 export class BuildNewStationComponent {
   hardwareComponents: string[] = [];
+  hardwareInstallationStep1: string[] = [];
+  hardwareInstallationStep2: string[] = [];
+  hardwareInstallationStep2A: string[] = [];
+  hardwareInstallationStep2B: string[] = [];
+
   constructor() {
+
     this.hardwareComponents.push('Veraboard', 'NodeMCU ESP8266 E-12', 'BME280 Sensor', 'TEMT6000 Light Sensor',
       '18650 TP4056 Lithium Battery Charger', 'Solar Panel (0.66W, 5.5V, 120mAh)', 'NCR18650A Battery (3.6V, 3100mAh)',
       '8 jumper cables', '2 copper wires', '7 male header pins', 'Soldering Iron & Soldering',
       'Computer thats not completely shyt', 'Working Internet');
+
+    this.hardwareInstallationStep1.push('Solder 4 male header pins to BME Sensor',
+      'Solder 3 male header pins to TEMT Light Sensor');
+
+    this.hardwareInstallationStep2.push('Connect 4 jumpers to BME Sensor and 3 to TEMT Light Sensor',
+      'Solder 3 male header pins to TEMT Light Sensor');
+
+    this.hardwareInstallationStep2A.push('Connect VIN (VCC) pin on BME Sensor to 3V3 (3.3V) ' +
+      'pin on the right side of the NodeMCU',
+      'Connect GND pin on BME Sensor to GND pin on the right side of the NodeMCU',
+      'Connect SCL pin on BME Sensor to D1 pin on the right side of the NodeMCU',
+      'Connect SDA pin on BME Sensor to D2 pin on the right side of the NodeMCU ');
+
+    this.hardwareInstallationStep2B.push('Connect S pin on TEMT Sensor to A000 pin on the left side of the NodeMCU',
+      'Connect G pin on TEMT Sensor to GND pin on the right side of the NodeMCU',
+      'Connect V pin on TEMT Sensor to 3V3 (3.3V) pin on the right side of the NodeMCU');
+
+
   }
   /*GoToPage(url: string) {
     window.open('www.github.com');
