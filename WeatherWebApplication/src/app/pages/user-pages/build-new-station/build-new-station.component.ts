e6eb81db31c35234502ea1729d9519481e2bf1e1
Eon VIljoen <28807995@student.g.nwu.ac.za>
2019-10-18 15:45:39 +0200

    Added hardware requirment checklist

--- WeatherWebApplication/src/app/pages/user-pages/build-new-station/build-new-station.component.ts
+++ WeatherWebApplication/src/app/pages/user-pages/build-new-station/build-new-station.component.ts
@@ -6,8 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['build-new-station.component.scss'],
 })
 export class BuildNewStationComponent {
-  constructor() {}
-  GoToPage(url: string) {
-    // window.open('www.github.com');
+  hardwareComponents: string[] = [];
+  constructor() {
+    this.hardwareComponents.push('Veraboard', 'NodeMCU ESP8266 E-12', 'BME280 Sensor', 'TEMT6000 Light Sensor',
+      '18650 TP4056 Lithium Battery Charger', 'Solar Panel (0.66W, 5.5V, 120mAh)', 'NCR18650A Battery (3.6V, 3100mAh)',
+      '8 jumper cables', '2 copper wires', '7 male header pins', 'Soldering Iron & Soldering',
+      'Computer thats not completely shyt', 'Working Internet');
   }
+  /*GoToPage(url: string) {
+    window.open('www.github.com');
+  }*/
 }
