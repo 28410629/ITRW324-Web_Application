43d7b059cb348635552deaa026857bdc7cc93f0a
Eon VIljoen <28807995@student.g.nwu.ac.za>
2019-10-19 21:43:21 +0200

    progressbar and checkboxes implementaiton

--- WeatherWebApplication/src/app/pages/user-pages/build-new-station/build-new-station.component.ts
+++ WeatherWebApplication/src/app/pages/user-pages/build-new-station/build-new-station.component.ts
@@ -1,26 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'ngx-build-new-station',
   templateUrl: 'build-new-station.component.html',
   styleUrls: ['build-new-station.component.scss'],
 })
-export class BuildNewStationComponent {
-  hardwareComponents: string[] = [];
+export class BuildNewStationComponent implements OnInit{
+  // To implement later
+/*  hardwareComponents: string[] = [];
   hardwareInstallationStep1: string[] = [];
   hardwareInstallationStep2: string[] = [];
   hardwareInstallationStep2A: string[] = [];
-  hardwareInstallationStep2B: string[] = [];
+  hardwareInstallationStep2B: string[] = [];*/
+
+  checkBoxArray: {
+    checkBoxId: number;
+    checked: boolean;
+  }[] = [];
 
   checked: boolean;
 
   progressBarValue: number;
   percentageComplete: number;
 
+  checkBoxId: number;
 
-  constructor() {
+  shittyId: string;
 
-    this.hardwareComponents.push('Veraboard', 'NodeMCU ESP8266 E-12', 'BME280 Sensor', 'TEMT6000 Light Sensor',
+  test1: boolean = false;
+
+  constructor(@Inject(DOCUMENT) document) {
+    // To implement later
+    /*this.hardwareComponents.push('Veraboard', 'NodeMCU ESP8266 E-12', 'BME280 Sensor', 'TEMT6000 Light Sensor',
       '18650 TP4056 Lithium Battery Charger', 'Solar Panel (0.66W, 5.5V, 120mAh)', 'NCR18650A Battery (3.6V, 3100mAh)',
       '8 jumper cables', '2 copper wires', '7 male header pins', 'Soldering Iron & Soldering',
       'Computer thats not completely shyt', 'Working Internet');
@@ -39,28 +51,67 @@ export class BuildNewStationComponent {
 
     this.hardwareInstallationStep2B.push('Connect S pin on TEMT Sensor to A000 pin on the left side of the NodeMCU',
       'Connect G pin on TEMT Sensor to GND pin on the right side of the NodeMCU',
-      'Connect V pin on TEMT Sensor to 3V3 (3.3V) pin on the right side of the NodeMCU');
+      'Connect V pin on TEMT Sensor to 3V3 (3.3V) pin on the right side of the NodeMCU');*/
 
     this.progressBarValue = 0;
     this.checked = false;
     this.percentageComplete = 0;
+    this.checkBoxId = 0;
+    this.shittyId = '';
   }
-  updateProgressbar() {
 
-  }
-
-  toggle(checked: boolean) {
-    this.checked = checked;
-    // this._sendDataToParent();
-    if (this.checked === true) {
-      this.percentageComplete = this.percentageComplete + 1;
-    } else {
-      this.percentageComplete = this.percentageComplete - 1;
+  ngOnInit(): void {
+    for (let i = 0; i < 29; i++) {
+      this.checkBoxArray.push({
+        checkBoxId: this.checkBoxId = this.GetCheckBoxId(),
+        checked: this.checked,
+      });
     }
   }
 
+  Toggle(event) {
+    this.checked = event.target.checked;
+    if (this.checked === true) {
+      this.percentageComplete = this.percentageComplete + ((1 / 29) * 100);
+      this.UpdateCheckBoxes(event.target.id, true);
+    } else {
+      this.percentageComplete = this.percentageComplete - ((1 / 29) * 100);
+      this.UpdateCheckBoxes(event.target.id, false);
+    }
+    //
+  }
 
+  UpdateCheckBoxes(id: number, good: boolean) {
+    if (good === true) {
+      for (let i = 0; i < this.checkBoxArray.length; i++) {
+          if (this.checkBoxArray[i].checkBoxId === (+id + +1)) {
+            this.checkBoxArray[i].checked = true;
+          }
+        }
+    }else {
+      for (let i = 0; i < this.checkBoxArray.length; i++) {
+          if (this.checkBoxArray[i].checkBoxId === (+id + +1)) {
+            this.checkBoxArray[i].checked = false;
+          }
+      }
+      }
+    console.log(this.checkBoxArray);
+    }
+
+  GetCheckBoxId() {
+      this.checkBoxId += 1;
+      return this.checkBoxId;
+  }
   /*GoToPage(url: string) {
     window.open('www.github.com');
+  }*/
+/*  showId(event) {
+    this.shittyId = event.target.id;
+    console.log(this.shittyId);
+  }
+
+  testCheck(event) {
+    console.log(event.target.id );
+
   }*/
 }
