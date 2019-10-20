3c5f378c2be4726b4de13a4331c14a93ec669ad7
Eon VIljoen <28807995@student.g.nwu.ac.za>
2019-10-20 14:55:43 +0200

    Firebase implementation + Morne

--- WeatherWebApplication/src/app/pages/user-pages/build-new-station/build-new-station.component.ts
+++ WeatherWebApplication/src/app/pages/user-pages/build-new-station/build-new-station.component.ts
@@ -1,129 +1,40 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {AuthService} from '../../../auth/auth-service.service';
+import {Subscription} from 'rxjs';
+import {User} from '../../../models/user.model';
 
 @Component({
   selector: 'ngx-build-new-station',
   templateUrl: 'build-new-station.component.html',
   styleUrls: ['build-new-station.component.scss'],
 })
-export class BuildNewStationComponent implements OnInit {
-  // To implement later
-/*  hardwareComponents: string[] = [];
-  hardwareInstallationStep1: string[] = [];
-  hardwareInstallationStep2: string[] = [];
-  hardwareInstallationStep2A: string[] = [];
-  hardwareInstallationStep2B: string[] = [];*/
-
-  /*checkBoxArray: {
-    checkBoxId: number;
-    checked: boolean;
-  }[] = [];*/
+export class BuildNewStationComponent implements OnInit, OnDestroy {
   checkBoxArray: boolean[] = [];
-
-  checked: boolean;
-
-  progressBarValue: number;
   percentageComplete: number;
-
-  checkBoxId: number;
-
-  shittyId: string;
-
-  test1: boolean = false;
-
-  constructor() {
-    // To implement later
-    /*this.hardwareComponents.push('Veraboard', 'NodeMCU ESP8266 E-12', 'BME280 Sensor', 'TEMT6000 Light Sensor',
-      '18650 TP4056 Lithium Battery Charger', 'Solar Panel (0.66W, 5.5V, 120mAh)', 'NCR18650A Battery (3.6V, 3100mAh)',
-      '8 jumper cables', '2 copper wires', '7 male header pins', 'Soldering Iron & Soldering',
-      'Computer thats not completely shyt', 'Working Internet');
-
-    this.hardwareInstallationStep1.push('Solder 4 male header pins to BME Sensor',
-      'Solder 3 male header pins to TEMT Light Sensor');
-
-    this.hardwareInstallationStep2.push('Connect 4 jumpers to BME Sensor and 3 to TEMT Light Sensor',
-      'Solder 3 male header pins to TEMT Light Sensor');
-
-    this.hardwareInstallationStep2A.push('Connect VIN (VCC) pin on BME Sensor to 3V3 (3.3V) ' +
-      'pin on the right side of the NodeMCU',
-      'Connect GND pin on BME Sensor to GND pin on the right side of the NodeMCU',
-      'Connect SCL pin on BME Sensor to D1 pin on the right side of the NodeMCU',
-      'Connect SDA pin on BME Sensor to D2 pin on the right side of the NodeMCU ');
-
-    this.hardwareInstallationStep2B.push('Connect S pin on TEMT Sensor to A000 pin on the left side of the NodeMCU',
-      'Connect G pin on TEMT Sensor to GND pin on the right side of the NodeMCU',
-      'Connect V pin on TEMT Sensor to 3V3 (3.3V) pin on the right side of the NodeMCU');*/
-
-    this.progressBarValue = 0;
-    this.checked = false;
+  userSubscription: Subscription;
+  useruid: string;
+  user: User;
+  constructor(private authService: AuthService) {
     this.percentageComplete = 0;
-    this.checkBoxId = 0;
-    this.shittyId = '';
+    const storageuser: User = JSON.parse(localStorage.getItem('user'));
+    this.useruid = storageuser.uid;
   }
 
   ngOnInit(): void {
-    for (let i = 0; i < 29; i++) {
-      this.checkBoxArray[i] = false;
-      /*this.checkBoxArray.push({
-        checkBoxId: this.checkBoxId = this.GetCheckBoxId(),
-        checked: this.checked,
-      });*/
-    }
-    this.checkBoxArray[0] = true;
+    this.userSubscription = this.authService.GetUserProfileData(this.useruid)
+      .subscribe(
+        responseData => {
+          this.user = responseData;
+          this.checkBoxArray = this.user.checkBoxArray;
+          this.updateProgressBar();
+        });
   }
-
-  Toggle(checked: boolean, id: number) {
-    this.checked = checked;
-    if (this.checked === true) {
-      this.percentageComplete = this.percentageComplete + ((1 / this.checkBoxArray.length) * 100);
-       this.UpdateCheckBoxes(id, true);
-    } else {
-      this.percentageComplete = this.percentageComplete - ((1 / this.checkBoxArray.length) * 100);
-       this.UpdateCheckBoxes(id, false);
-    }
-    //
-  }
-
-  UpdateCheckBoxes(id: number, good: boolean) {
-    if (good === true) {
-      for (let i = 0; i < this.checkBoxArray.length; i++) {
-        if (id === i) {
-          this.checkBoxArray[i] = true;
-        }
-          /*if (this.checkBoxArray[i].checkBoxId === (+id + +1)) {
-            this.checkBoxArray[i].checked = true;
-          }*/
-        }
-    }else {
-      for (let i = 0; i < this.checkBoxArray.length; i++) {
-        if (id === i) {
-          this.checkBoxArray[i] = false;
-        }
-          /*if (this.checkBoxArray[i].checkBoxId === (+id + +1)) {
-            this.checkBoxArray[i].checked = false;*/
-          }
-      }
-    console.log(this.checkBoxArray);
-  }
-
-  GetCheckBoxId() {
-      this.checkBoxId += 1;
-      return this.checkBoxId;
-  }
-  /*GoToPage(url: string) {
-    window.open('www.github.com');
-  }*/
-  showId(event) {
-    this.shittyId = event.target.id;
-    console.log(this.shittyId);
-  }
-
-  getData() {
-
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
   }
 
   saveToFirebase() {
-    // call method to save aray to fb
-    console.log(this.checkBoxArray);
+    this.authService.UpdateUserProgress(this.checkBoxArray);
     this.updateProgressBar();
   }
 
