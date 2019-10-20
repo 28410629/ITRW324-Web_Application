cf8db154b73c771cbbfde46b7f98e5036ff78f81
Coenraad Human <coen.human@gmail.com>
2019-10-20 18:52:11 +0200

    Started with register station.

--- WeatherWebApplication/src/app/pages/user-pages/register-new-station/register-new-station.component.ts
+++ WeatherWebApplication/src/app/pages/user-pages/register-new-station/register-new-station.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {LocationUtilities} from '../../../common/location.utilities';
 
 @Component({
   selector: 'ngx-register-new-station',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['register-new-station.component.scss'],
 })
 export class RegisterNewStationComponent {
-  constructor() {}
+  countries;
+  selectedCountry;
+  provinces;
+  selectedProvince;
+  cities;
+  selectedCity;
+  constructor(private locationUtil: LocationUtilities) {
+    // get available stations
+    this.getLocationList();
+  }
+  // this.source.load(data);
+
+  getLocationList() {
+    this.countries = this.locationUtil.getCountry();
+    this.selectedCountry = this.countries[0];
+    this.provinces = this.locationUtil.getProvinceList(this.countries[0]);
+    this.selectedProvince = this.provinces[0];
+    this.populateCities(this.provinces[0]);
+  }
+  populateCities(name) {
+    this.selectedProvince = name;
+    this.cities = this.locationUtil.getCityList(this.countries[0], this.selectedProvince);
+    this.selectedCity = this.cities[0];
+  }
 }
