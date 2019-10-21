881c15056d6071f1726e3c54fa7b012519949f56
Coenraad Human <coen.human@gmail.com>
2019-10-21 08:04:08 +0200

    Ts lint fix.

--- WeatherWebApplication/src/app/services/register-new-station.service.ts
+++ WeatherWebApplication/src/app/services/register-new-station.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
-import {FetchJsonUtilities} from '../common/fetch-json.utilities';
 
 @Injectable()
 export class RegisterNewStationService {
-  constructor(private common: FetchJsonUtilities) {
+  constructor() {
   }
 }
