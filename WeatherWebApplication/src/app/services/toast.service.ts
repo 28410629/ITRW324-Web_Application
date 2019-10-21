25975a4b91f8ecd19c204e0c379ce012489d661b
Coenraad Human <coen.human@gmail.com>
2019-10-21 08:41:57 +0200

    Added info toast as well.

--- WeatherWebApplication/src/app/services/toast.service.ts
+++ WeatherWebApplication/src/app/services/toast.service.ts
@@ -3,15 +3,21 @@ import {NbToastrService} from '@nebular/theme';
 
 @Injectable()
 export class ToastService {
+  toastPosition = 'top-right';
+
   constructor( private toastrService: NbToastrService) {
   }
 
   public ShowSuccessToast(title, message) {
-    this.SuccessToast('top-right', title, message);
+    this.SuccessToast(this.toastPosition, title, message);
   }
 
   public ShowFailedToast(title, message) {
-    this.FailedToast('top-right', title, message);
+    this.FailedToast(this.toastPosition, title, message);
+  }
+
+  public ShowInfoToast(title, message) {
+    this.InfoToast(this.toastPosition, title, message);
   }
 
   private SuccessToast(position, title, message) {
@@ -26,5 +32,12 @@ export class ToastService {
       message,
       title,
       { position: position, status: 'warning' });
+  }
+
+  private InfoToast(position, title, message) {
+    this.toastrService.show(
+      message,
+      title,
+      { position: position, status: 'info' });
   }
 }
