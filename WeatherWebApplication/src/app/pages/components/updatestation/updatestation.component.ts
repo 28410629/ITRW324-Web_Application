8d1c03a37e89ec18a53ddca9225cc36a02b24515
Morne <mornneventer.mv@gmail.com>
2019-10-22 17:12:17 +0200

    new componenet fix

--- WeatherWebApplication/src/app/pages/components/updatestation/updatestation.component.ts
+++ WeatherWebApplication/src/app/pages/components/updatestation/updatestation.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {LocationUtilities} from '../../../common/location.utilities';
+import {ToastService} from '../../../services/toast.service';
 
 @Component({
   selector: 'ngx-updatestations',
@@ -10,12 +12,44 @@ import {Router} from '@angular/router';
 export class UpdatestationComponent implements OnInit {
 
   isLoaded = false;
-
-  constructor(public router: Router) {
+  countries;
+  selectedCountry;
+  provinces;
+  selectedProvince;
+  cities;
+  selectedCity;
+  success: boolean = false;
+  loading: boolean = false;
+  constructor(public router: Router,
+              private locationUtil: LocationUtilities,
+              private toastService: ToastService) {
+    this.getLocationList();
   }
 
   ngOnInit() {
     this.isLoaded = true;
+  }
+
+  getLocationList() {
+    this.countries = this.locationUtil.getCountry();
+    this.selectedCountry = this.countries[0];
+    this.provinces = this.locationUtil.getProvinceList(this.countries[0]);
+    this.selectedProvince = this.provinces[0];
+    this.populateCities(this.provinces[0]);
+  }
+  populateCities(name) {
+    this.selectedProvince = name;
+    this.cities = this.locationUtil.getCityList(this.countries[0], this.selectedProvince);
+    this.selectedCity = this.cities[0];
+  }
+  updateStation() {
+    if (!this.loading) {
+      this.loading = true;
+      this.toastService.ShowSuccessToast('Register New Station', 'Successfully added station to the system.');
+    }
+  }
+  onClose() {
+    this.success = false;
   }
 
 }
--- WeatherWebApplication/src/app/pages/user-pages/manage-your-stations/manage-your-stations.component.ts
+++ WeatherWebApplication/src/app/pages/user-pages/manage-your-stations/manage-your-stations.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import {LocationUtilities} from '../../../common/location.utilities';
-import {ToastService} from '../../../services/toast.service';
 
 @Component({
   selector: 'ngx-register-new-station',
@@ -8,38 +6,8 @@ import {ToastService} from '../../../services/toast.service';
   styleUrls: ['manage-your-stations.component.scss'],
 })
 export class ManageYourStationsComponent {
-  countries;
-  selectedCountry;
-  provinces;
-  selectedProvince;
-  cities;
-  selectedCity;
-  success: boolean = false;
-  loading: boolean = false;
-  constructor(private locationUtil: LocationUtilities,
-              private toastService: ToastService) {
-    // get available stations
-    this.getLocationList();
+
+  constructor() {
   }
-  getLocationList() {
-    this.countries = this.locationUtil.getCountry();
-    this.selectedCountry = this.countries[0];
-    this.provinces = this.locationUtil.getProvinceList(this.countries[0]);
-    this.selectedProvince = this.provinces[0];
-    this.populateCities(this.provinces[0]);
-  }
-  populateCities(name) {
-    this.selectedProvince = name;
-    this.cities = this.locationUtil.getCityList(this.countries[0], this.selectedProvince);
-    this.selectedCity = this.cities[0];
-  }
-  updateStation() {
-    if (!this.loading) {
-      this.loading = true;
-      this.toastService.ShowSuccessToast('Register New Station', 'Successfully added station to the system.');
-    }
-  }
-  onClose() {
-    this.success = false;
-  }
+
 }
